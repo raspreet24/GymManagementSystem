20f41757d7c14657dcb5b8e24ffafeecef31978a
raspreet <kourraspreet67@gmail.com>
2025-05-06 15:27:14 +0530

    Db Fix

--- DBConn/conn.js
+++ DBConn/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/gymBackend')
+mongoose.connect('mongodb+srv://kourraspreet67:J7zVMEwVwxr2dV2U@cluster0.gwbiobu.mongodb.net/gymBackend')
 .then(() => console.log('DB connection successful!')).catch(err=>{
     console.log(err)
 });
